a6cda28a506d38f8082f4fea546f3b291b60688d
shaileeM <shailee.madhani@gmail.com>
2016-02-28 03:11:02 -0800

    First commit

---
+++ questions.js
@@ -0,0 +1,114 @@
+alert("...");
+var ques=[[{
+        question: "Where is the great Barrier Reef?",
+        questionType: 1,
+        choices: ["Africa", "North America", "Asia", "Australia"],
+        correctChoice: "Australia",
+        score:5,
+        hint:"It has one of the best beaches in the World."
+    },
+    {
+        question: "What planet is the Curiosity Rover on now?",
+        questionType: 2,
+        correctAnswer: "Mars",
+        score:10,
+        hint:"It has one of the best beaches in the World."
+    },
+    {
+        question: "What is the highest mountain on Earth?",
+        questionType: 2,
+        correctAnswer: "Mount Everest",
+        score:10,
+        hint:"It has one of the best beaches in the World."
+    },
+    {
+        question: "Where are lines of longitude farthest apart?",
+        questionType:1,
+        choices:["At the Prime Meridian", "At the Equator", "At the Date Line","At the South Pole"],
+        correctChoice:"At the Equator",
+        score:5,
+        hint:"It has one of the best beaches in the World."
+    },
+    {
+        question: "What is the capital of India?",
+        questionType:1,
+        choices:[ "Mumbai", "Beijing", "London","New Delhi"],
+        correctChoice:"New Delhi",
+        score:5,
+        hint:"It has one of the best beaches in the World."
+    }],[{
+        question:"The Battle of Plassey was fought in?",
+        questionType:1,
+        choices:["1757", "1782", "1748", "1764"],
+        correctChoice:"1757",
+        score:5,
+        hint:"The battle took place at Palashi, Bengal, on the river banks of the Bhagirathi River, about 150 km north of Calcutta, near Murshidabad, then capital of undivided Bengal."
+    },
+    {
+        question:"Under Akbar, the Mir Bakshi was required to look after",
+        questionType: 2,
+        correctAnswer: "Military Affairs",
+        score:10,
+        hint:"The Mir Bakshi was in charge of intelligence gathering, and also made recommendations to the emperor for military appointments and promotions."
+    },
+    {
+        question: "The victories of Karikala are well portrayed in",
+        questionType: 2,
+        correctAnswer: "Pattinappalai",
+        score:10,
+        hint:"It has one of the best beaches in the World."
+    },
+    {
+        question: "Todar Mal was associated with",
+        questionType:1,
+        choices:["music", "literature", "finance", "law"],
+        correctChoice: "finance",
+        score:5,
+        hint:"Raja Todar Mal was the Finance Minister of the Mughal empire during Akbar's reign. He was one of the Navaratnas in Akbar's durbar."
+    },
+    {
+        question: "The weekly Commonweal was founded by?",
+        questionType:1,
+        choices: ["Annie Besant", "Bipan Chandra Pal", "Bal Gangadhar Tilak","Sarojini Naidu"],
+        correctChoice: "1939",
+        score:5,
+        hint:"The person has won the Bharat Ratna Award."
+    }],[{
+        question: "What kind of waves are used to make and receive cellphone call?",
+        questionType:1,
+        choices:["Radio waves", "Visible light waves", "Sound waves", "Gravity waves"],
+        correctChoice:"Radio waves",
+        score:5,
+        hint:"It has one of the best beaches in the World."
+    },
+    {
+        question: "What does a light-year measure?",
+        questionType: 2,
+        correctAnswer: "Distance",
+        score:10,
+        hint:"It has one of the best beaches in the World."
+    },
+    {
+        question: "What element is needed to make nuclear energy and nuclear weapons?",
+        questionType: 2,
+        correctAnswer: "Uranium",
+        score:10,
+        hint:"It has one of the best beaches in the World."
+    },
+    {
+        question: "Chlorophyll is a naturally occurring chelate compound in which central metal is",
+        questionType:1,
+        choices:["  copper", "magnesium", "iron","calcium"],
+        correctChoice: "magnesium",
+        score:5,
+        hint:"It is a water disinfectant"
+    },
+    {
+        question: "Which of the following is used in pencils?",
+        questionType:1,
+        choices: ["graphite", "silicon", "charcoal", "phosphorous"],
+        correctChoice: "graphite",
+        score:5,
+        hint:"Main ingredient of Marble."
+    }]];
+
